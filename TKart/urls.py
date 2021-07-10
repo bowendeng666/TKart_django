cece5822911a8ef6604bd97c070c502cc7020509
bowendeng666 <bd45@rice.edu>
2021-07-10 14:44:40 +0800

    add store model and template

--- TKart/urls.py
+++ TKart/urls.py
@@ -15,11 +15,13 @@ Including another URLconf
 """
 from os import stat
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.urls.conf import include
 from . import views
 from django.conf.urls.static import static
 from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.home, name='home'),
+    path('store/', include('store.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) # add url support for media file
