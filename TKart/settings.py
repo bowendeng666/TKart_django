bd8602fd9a2b67dfc70dc37f34f54c11c540a9be
bowendeng666 <bd45@rice.edu>
2021-08-06 18:39:53 -0500

    settings changed

--- TKart/settings.py
+++ TKart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True,cast=bool) #bring it as boolean value
 
-ALLOWED_HOSTS = ['TKart-app-env.eba-4xuxwmsv.us-west-2.elasticbeanstalk.com']
+ALLOWED_HOSTS = []
 
 
 # Application definition
