03d61809d7ce9936888a8f90841dfc385624c191
bowendeng666 <bd45@rice.edu>
2021-08-07 11:12:10 -0500

    add host

--- TKart/settings.py
+++ TKart/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True,cast=bool) #bring it as boolean value
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['TKart-app.eba-jkgb2x8p.us-west-2.elasticbeanstalk.com']
 # ALLOWED_HOSTS = []
 
 
